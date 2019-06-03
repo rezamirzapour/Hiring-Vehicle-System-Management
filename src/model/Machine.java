c6f89cafed90abb6862f2d725cef099bf9d3c281
RezaMirzapour <mirzaandrampage@gmail.com>
2019-06-03 16:58:31 -0700

    add type of vehicle to machine

--- src/model/Machine.java
+++ src/model/Machine.java
@@ -1,6 +1,9 @@
 package model;
 
 public class Machine extends Vehicle {
+    private enum type {
+        Sport, Economic, Luxury;
+    }
     @Override
     public int getBasicPrice() {
         return 1500;
