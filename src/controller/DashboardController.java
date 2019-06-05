4ff96d59bca06f6ef120cf9f7e3da5ecfd6a2905
RezaMirzapour <mirzaandrampage@gmail.com>
2019-06-05 00:48:37 -0700

    add some vehicles list seed data

--- src/controller/DashboardController.java
+++ src/controller/DashboardController.java
@@ -2,14 +2,21 @@ package controller;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import model.Garage;
 import model.vehicle.Vehicle;
 
+import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.net.URL;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class DashboardController implements Initializable {
@@ -19,7 +26,7 @@ public class DashboardController implements Initializable {
     private TableView<Vehicle> tbData;
 
     @FXML
-    private TableColumn<Vehicle, Integer > id;
+    private TableColumn<Vehicle, Integer> id;
 
     @FXML
     private TableColumn<Vehicle, String> model;
@@ -33,12 +40,17 @@ public class DashboardController implements Initializable {
     @FXML
     private TableColumn<Vehicle, String> description;
 
+    @FXML
+    private ComboBox<Integer> garageSelector;
+
+
     public void initialize(URL location, ResourceBundle resources) {
-        loadVehicles();
+        loadVehicles(garages.get(0));
         loadFromFile();
     }
 
-    private ObservableList<Vehicle> vehiclesList = FXCollections.observableArrayList(
+
+    private ObservableList<Vehicle> vehiclesList1 = FXCollections.observableArrayList(
             new Vehicle("Pride", "factory1", 2016, "Description1"),
             new Vehicle("Tiba", "factory2", 2017, "Description2"),
             new Vehicle("Tondar", "factory3", 2018, "Description3"),
@@ -46,15 +58,38 @@ public class DashboardController implements Initializable {
 
     );
 
+    private ObservableList<Vehicle> vehiclesList2 = FXCollections.observableArrayList(
+            new Vehicle("Tiba", "factory2", 2017, "Description2"),
+            new Vehicle("Tondar", "factory3", 2018, "Description3"),
+            new Vehicle("Jack", "factory4", 2019, "Description4"),
+            new Vehicle("Tiba", "factory2", 2017, "Description2"),
+            new Vehicle("Pride", "factory1", 2016, "Description1")
+    );
 
-    private void loadVehicles()
-    {
+    private ObservableList<Vehicle> vehiclesList3 = FXCollections.observableArrayList(
+            new Vehicle("Tondar", "factory3", 2018, "Description3"),
+            new Vehicle("Jack", "factory4", 2019, "Description4"),
+            new Vehicle("Tiba", "factory2", 2017, "Description2"),
+            new Vehicle("Pride", "factory1", 2016, "Description1"),
+            new Vehicle("Tiba", "factory2", 2017, "Description2")
+    );
+
+    private ObservableList<Garage> garages = FXCollections.observableArrayList(
+            new Garage(vehiclesList1),
+            new Garage(vehiclesList2),
+            new Garage(vehiclesList3)
+    );
+
+
+    private void loadVehicles(Garage garage) {
         id.setCellValueFactory(new PropertyValueFactory<>("id"));
         model.setCellValueFactory(new PropertyValueFactory<>("model"));
         factory.setCellValueFactory(new PropertyValueFactory<>("factory"));
         createYear.setCellValueFactory(new PropertyValueFactory<>("createYear"));
         description.setCellValueFactory(new PropertyValueFactory<>("description"));
-        tbData.setItems(vehiclesList);
+        for (int i = 0; i < garage.getVehicles().size(); i++) {
+            tbData.getItems().add(garage.getVehicles().get(i));
+        }
     }
 
     private void loadFromFile() {
