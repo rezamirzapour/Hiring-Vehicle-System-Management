f524257b31e31b7d817f6666688f72b89d61ce5c
RezaMirzapour <mirzaandrampage@gmail.com>
2019-06-04 23:29:18 -0700

    some security changes

--- src/model/Management.java
+++ src/model/Management.java
@@ -8,7 +8,25 @@ public class Management {
     private List<Garage> garageList;
 
     public Management(int storeCount, int garageCount) {
-        this.storeList = new ArrayList<>(storeCount);
-        this.garageList = new ArrayList<>(garageCount);
+        setStoreList(new ArrayList<>(garageCount));
+        setStoreList(new ArrayList<>(storeCount));
+    }
+
+
+
+    public void setGarageList(List<Garage> garageList) {
+        this.garageList = garageList;
+    }
+
+    public void setStoreList(List<Store> storeList) {
+        this.storeList = storeList;
+    }
+
+    public List<Garage> getGarageList() {
+        return garageList;
+    }
+
+    public List<Store> getStoreList() {
+        return storeList;
     }
 }
