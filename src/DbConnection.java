008509e6d001766bd0fc3e14ec8e8f2426af7f0e
RezaMirzapour <mirzaandrampage@gmail.com>
2019-06-10 18:51:00 -0700

    add insert vehicle method

--- src/DbConnection.java
+++ src/DbConnection.java
@@ -26,13 +26,25 @@ public class DbConnection {
     }
 
     public void createTable(){
-        String tablSQL="CREATE TABLE IF NOT EXISTS vehicles (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL ,model TEXT , factory TEXT, createYear INTEGER, description TEXT);";
+        String tablSQL="CREATE TABLE IF NOT EXISTS vehicles (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL ,model TEXT , factory TEXT, createYear INTEGER, description TEXT, vehicleType TEXT);";
         try {
             st.executeUpdate(tablSQL);
-            System.out.println("person TABLE created");
+            System.out.println("vehicles TABLE created");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    public void insertVehicle(String model,String factory, int createYear, String description, String vehicleType){
+        String insertSQL="INSERT INTO vehicles (model,factory) VALUES ('"+model+"','"+factory+"','"+createYear+"','"+description+"','"+vehicleType+"');";
+        try {
+            st.executeUpdate(insertSQL);
+            System.out.println("Inserted!");
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
     }
 
     public void close() {
