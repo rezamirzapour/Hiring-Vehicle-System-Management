eb30f268c87ed25aa02491c0ec57e65afa3a0226
RezaMirzapour <mirzaandrampage@gmail.com>
2019-06-10 18:12:57 -0700

    define connect and close methods

--- src/DbConnection.java
+++ src/DbConnection.java
@@ -4,6 +4,31 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbConnection {
+    Connection c;
 
+    public DbConnection() {
+        connect();
+        close();
+    }
+
+    public void connect() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:advancedjava.db");
+            System.out.println("Connection Created!");
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void close() {
+
+        try {
+            c.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
 }
