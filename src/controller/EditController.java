33bea08f171c671ed1160391b5d4f49db7c7efa5
RezaMirzapour <mirzaandrampage@gmail.com>
2019-06-11 21:12:07 -0700

    perform update operation

--- src/controller/EditController.java
+++ src/controller/EditController.java
@@ -1,5 +1,99 @@
 package controller;
 
-public class EditController extends Controller {
+import database.DbConnection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import model.Garage;
+import model.vehicle.Bus;
+import model.vehicle.Machine;
+import model.vehicle.Vehicle;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class EditController extends Controller {
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private TableColumn<?, ?> id;
+
+    @FXML
+    private TableColumn<?, ?> model;
+
+    @FXML
+    private TableColumn<?, ?> factory;
+
+    @FXML
+    private TableColumn<?, ?> createYear;
+
+    @FXML
+    private TableColumn<?, ?> description;
+
+    @FXML
+    private TableView<Vehicle> tbData;
+
+    @FXML
+    private ComboBox<Integer> garageSelector;
+
+    @FXML
+    private Button editButton;
+
+    @FXML
+    private TextField modelTextField;
+
+    @FXML
+    private TextField factoryTextField;
+
+    @FXML
+    private TextField createTextField;
+
+    @FXML
+    private TextField descriptionTextField;
+
+    @FXML
+    private ComboBox<String> vehicleType;
+
+
+    public void initialize(URL location, ResourceBundle resources) {
+        loadGarageSelector(garageSelector);
+        loadVehicles(Integer.parseInt(garageSelector.getValue().toString()), tbData, id, model, factory, createYear, description);
+        loadBasicType(vehicleType);
+        editButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                DbConnection db = new DbConnection();
+                db.updateVehicle(
+                        tbData.getSelectionModel().getSelectedItem().getId(),
+                        modelTextField.getText(),
+                        factoryTextField.getText(),
+                        Integer.parseInt(createTextField.getText()),
+                        descriptionTextField.getText(),
+                        vehicleType.getSelectionModel().getSelectedItem()
+                );
+                db.close();
+                loadDashboardScene();
+            }
+        });
+    }
+
+    private void loadDashboardScene() {
+        try {
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("../view/Dashboard.fxml"));
+            anchorPane.getChildren().removeAll();
+            anchorPane.getChildren().setAll(pane);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
